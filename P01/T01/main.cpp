f33ea867c29585b2ee386ba6caf7e0ff6d4edf00
Assassin Devon <assassid@student.21-school.ru>
2026-09-11 18:16:39 +0300

    Init commit

--- P01/T01/main.cpp
+++ P01/T01/main.cpp
@@ -32,13 +32,52 @@ bool contains(const int a[], int n, int x)
     return false;
 }
 
-int main() 
+int main()
 {
-    int a[] = {4, 7, 11};
+    {
+        int a[] = {};
 
-    cout << max_element_value(a, 3) << endl;
-    cout << sum_elements(a, 3) << endl;
-    cout << contains(a, 3, 7) << endl;
+        cout << "=== Тест 1 ===" << endl;
+        cout << "Максимум: не вызываем" << endl;
+        cout << "Сумма: " << sum_elements(a, 0) << endl;
+        cout << "Проверка contains: " << contains(a, 0, 5) << endl;
+    }
+
+    {
+        int a[] = {5};
+
+        cout << "=== Тест 2 ===" << endl;
+        cout << "Максимум: " << max_element_value(a, 1) << endl;
+        cout << "Сумма: " << sum_elements(a, 1) << endl;
+        cout << "Проверка contains: " << contains(a, 1, 5) << endl;
+    }
+
+    {
+        int a[] = {-8, -3, -10};
+
+        cout << "=== Тест 3 ===" << endl;
+        cout << "Максимум: " << max_element_value(a, 3) << endl;
+        cout << "Сумма: " << sum_elements(a, 3) << endl;
+        cout << "Проверка contains: " << contains(a, 3, -3) << endl;
+    }
+
+    {
+        int a[] = {4, 7, 11};
+
+        cout << "=== Тест 4 ===" << endl;
+        cout << "Максимум: " << max_element_value(a, 3) << endl;
+        cout << "Сумма: " << sum_elements(a, 3) << endl;
+        cout << "Проверка contains: " << contains(a, 3, 6) << endl;
+    }
+
+    {
+        int a[] = {7, 7, 9};
+
+        cout << "=== Тест 5 ===" << endl;
+        cout << "Максимум: " << max_element_value(a, 3) << endl;
+        cout << "Сумма: " << sum_elements(a, 3) << endl;
+        cout << "Проверка contains: " << contains(a, 3, 7) << endl;
+    }
 
     return 0;
 }
