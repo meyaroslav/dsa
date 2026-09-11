cea3afbcbb9b5997430c5640636b5e203d7668ca
Assassin Devon <assassid@student.21-school.ru>
2026-09-11 17:51:42 +0300

    Init commit

--- P01/T01/main.cpp
+++ P01/T01/main.cpp
@@ -2,54 +2,43 @@
 
 using namespace std;
 
-int array_max(int arr[], int n) 
+int max_element_value(const int a[], int n)
 {
-    int max = arr[0];
+    int max = a[0];
 
-    for (int i = 1; i < n; i++) 
-    {
-        if (arr[i] > max) 
-        {
-            max = arr[i];
-        }
-    }
+    for (int i = 1; i < n; i++)
+        if (a[i] > max)
+            max = a[i];
 
     return max;
 }
 
-int array_sum(int arr[], int n) 
+long long sum_elements(const int a[], int n)
 {
-    int sum = 0;
+    long long sum = 0;
 
-    for (int i = 1; i < n; i++)
-    {
-        sum += arr[i];
-    }
+    for (int i = 0; i < n; i++)
+        sum += a[i];
 
     return sum;
 }
 
-bool array_check(int arr[], int n, int a)
+bool contains(const int a[], int n, int x)
 {
-    for (int i = 1; i < n; i++)
-    {
-        if (arr[i] == a) 
-        {
+    for (int i = 0; i < n; i++)
+        if (a[i] == x)
             return true;
-        }
-    }
 
     return false;
 }
 
 int main() 
 {
-    int arr[] = {1, 2, 3, 4, 5};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int a[] = {4, 7, 11};
 
-    cout << "Максимальный элемент массива: " << array_max(arr, n) << endl;
-    cout << "Сумма всех элементов массива: " << array_sum(arr, n) << endl;
-    cout << "Проверка заданного числа: " << (array_check(arr, n, 6) ? "да" : "нет") << endl;
+    cout << max_element_value(a, 3) << endl;
+    cout << sum_elements(a, 3) << endl;
+    cout << contains(a, 3, 7) << endl;
 
     return 0;
 }
