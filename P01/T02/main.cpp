89517a7e8a087a4c757eaf23bcbe956ab1c7f231
Assassin Devon <assassid@student.21-school.ru>
2026-09-25 17:15:59 +0300

    Init commit

--- P01/T02/main.cpp
+++ P01/T02/main.cpp
@@ -2,7 +2,59 @@
 
 using namespace std;
 
+void sort_bubble(int a[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        bool swapped = false;
+
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+                swapped = true;
+            }
+        }
+
+        if (!swapped)
+            break;
+    }
+}
+
+void sort_insert(int a[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int key = a[i];
+        int j = i - 1;
+
+        while (j >= 0 && a[j] > key)
+        {
+            a[j + 1] = a[j];
+            j--;
+        }
+
+        a[j + 1] = key;
+    }
+}
+
 int main()
 {
+    int n;
+    cin >> n;
 
+    int a[2000];
+
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    sort_bubble(a, n);
+
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
+
+    return 0;
 }
